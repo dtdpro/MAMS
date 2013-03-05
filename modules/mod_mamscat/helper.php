b7462cec2cabf816ce587c0f952f56b0297e1c90
Mike  Amundsen - TLG <mamundsen@the-lynx-group.com>
2013-03-05 16:58:53 -0500

    Lateset Category Module

---
+++ modules/mod_mamscat/helper.php
@@ -0,0 +1,77 @@
+<?php
+
+
+// no direct access
+defined('_JEXEC') or die;
+
+class modMAMSCatHelper
+{
+	static function getFeatured($params)
+	{
+		$db		= JFactory::getDbo();
+		$user = JFactory::getUser();
+		$cfg = MAMSHelper::getConfig();
+		
+		$alvls = Array();
+		$alvls = $user->getAuthorisedViewLevels();
+		$alvls = array_merge($alvls,$cfg->reggroup);
+		
+		$qcat = $db->getQuery(true);
+		$qcat->select('ac.ac_art');
+		$qcat->from('#__mams_artcat AS ac');
+		$qcat->where('ac.ac_cat = '.(int)$params->get('catid'));
+		$qcat->where('ac.published >= 1');
+		$db->setQuery($qcat);
+		$catids = $db->loadResultArray(0);
+		
+		
+		$query	= $db->getQuery(true);
+
+		$query->select('a.*,s.*');
+		$query->from('#__mams_articles as a');
+		$query->join('RIGHT','#__mams_secs AS s ON s.sec_id = a.art_sec');
+		$query->where('a.art_id IN ('.implode(",",$catids).')');
+		$query->where('a.access IN ('.implode(",",$alvls).')');
+		$query->where('a.published >= 1');
+		if (!in_array($cfg->ovgroup,$alvls)) $query->where('a.art_published <= NOW()');
+		$query->order('a.art_published DESC');
+		$db->setQuery($query,0,$params->get('count',5));
+		$items = $db->loadObjectList();
+		
+		
+		
+		
+		//Get Authors
+		foreach ($items as &$i) {
+			$qa=$db->getQuery(true);
+			$qa->select('a.auth_id,a.auth_name,a.auth_alias,a.auth_sec');
+			$qa->from('#__mams_artauth as aa');
+			$qa->join('RIGHT','#__mams_authors AS a ON aa.aa_auth = a.auth_id');
+			$qa->where('aa.published >= 1');
+			$qa->where('a.published >= 1');
+			$qa->where('a.access IN ('.implode(",",$alvls).')');
+			$qa->where('aa.aa_art = '.$i->art_id);
+			$qa->order('aa.ordering ASC');
+			$db->setQuery($qa);
+			$i->auts=$db->loadObjectList();
+		}
+		
+		//Get Cats
+		foreach ($items as &$i) {
+			$qc=$db->getQuery(true);
+			$qc->select('c.cat_id,c.cat_title,c.cat_alias');
+			$qc->from('#__mams_artcat as ac');
+			$qc->join('RIGHT','#__mams_cats AS c ON ac.ac_cat = c.cat_id');
+			$qc->where('ac.published >= 1');
+			$qc->where('c.published >= 1');
+			$qc->where('c.access IN ('.implode(",",$alvls).')');
+			$qc->where('ac.ac_art = '.$i->art_id);
+			$qc->order('ac.ordering ASC');
+			$db->setQuery($qc);
+			$i->cats=$db->loadObjectList();
+		}
+		
+		return $items;
+	}
+
+}
---
+++ modules/mod_mamscat/tmpl/default.php
@@ -0,0 +1,80 @@
+<?php
+
+// no direct access
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.keepalive');
+echo '<ul class="mams-featmod-list">';
+foreach ($articles as $a) {
+	echo '<li class="mams-featmod-listitem">';
+	echo '<div class="mams-featmod-title">';
+	echo '<a href="'.JRoute::_("index.php?option=com_mams&view=article&secid=".$a->sec_id.":".$a->sec_alias."&artid=".$a->art_id.":".$a->art_alias).'">';
+	echo $a->art_title;
+	echo '</a></div>';
+	if ($a->auts && $params->get('show_author',1)) {
+		$auts = Array();
+		foreach ($a->auts as $f) {
+			if ($params->get('link_pubinfo',0)) $auts[]='<a href="'.JRoute::_("index.php?option=com_mams&view=author&secid=".$f->auth_sec."&autid=".$f->auth_id.":".$f->auth_alias).'" class="mams-artlist-autlink">'.$f->auth_name.'</a>';
+			else $auts[]=$f->auth_name;
+		}
+		echo '<br /><div class="mams-featmod-author">';
+		echo implode(", ",$auts);
+		echo '</div>';
+	}
+	if ($params->get('show_pubinfo',1)) {
+		//Section Link
+		echo '<div class="mams-featmod-pubinfo">';
+		if ($params->get('show_pubsec',1)) {
+			
+			echo '<span class="mams-featmod-sec">';
+			if ($params->get('link_pubinfo',0)) echo '<a href="'.JRoute::_("index.php?option=com_mams&view=artlist&layout=section&secid=".$a->sec_id.":".$a->sec_alias).'" class="mams-artlist-seclink">';
+			echo '<em>'.$a->sec_name.'</em>';
+			if ($params->get('link_pubinfo',0)) echo '</a>';
+			echo '</span>';
+		}
+		
+		//Pub Date
+		if ($params->get('show_pubdate',1)) {
+			echo '<span class="mams-featmod-pubdate">';
+			echo ' published on <strong>';
+			echo date("F j, Y",strtotime($a->art_published));
+			echo '</strong>';
+			echo '</span>';
+		}
+		
+		//Cat Links
+		if ($a->cats && $params->get('show_pubcat',1)) {
+			echo '<span class="mams-featmod-cat">';
+			if ($params->get('show_pubdate',1)) {
+				echo ' in <em>';
+			} else {
+				echo ' - <em>';
+			}
+			$cats = Array();
+			foreach ($a->cats as $c) {
+				if ($params->get('link_pubinfo',0)) $cats[]='<a href="'.JRoute::_("index.php?option=com_mams&view=artlist&layout=category&secid=".$a->sec_id.":".$a->sec_alias."&catid=".$c->cat_id.":".$c->cat_alias).'" class="mams-artlist-catlink">'.$c->cat_title.'</a>';
+				else $cats[]=$c->cat_title;
+			}
+			echo implode(", ",$cats);
+			echo '</em>';
+			echo '</span>';
+		}
+		echo '</div>';
+	}
+		
+	//Description
+	if ($params->get('show_desc',0)) {
+		echo '<div class="mams-featmod-desc">';
+		echo $a->art_desc;
+		echo '</div>';
+	}
+	//Readmore
+	if ($params->get('show_readmore',0)) {
+		echo '<div class="mams-featmod-readmore">';
+		echo '<a href="'.JRoute::_("index.php?option=com_mams&view=article&secid=".$a->sec_id.":".$a->sec_alias."&artid=".$a->art_id.":".$a->art_alias).'" class="mams-featmod-artlink read-more">';
+		echo $params->get('text_readmore',"Read More");
+		echo '</a></div>';
+	}
+	echo '</li>';
+}
+echo '</ul>';
