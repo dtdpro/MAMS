e91afec83f013954ddbf0ed0218891f64e32fc96
Mike  Amundsen - TLG <mamundsen@the-lynx-group.com>
2014-01-28 11:24:07 -0500

    Update SQL Fix

--- component/admin/sql/updates/2.0.0.sql
+++ component/admin/sql/updates/2.0.0.sql
@@ -18,8 +18,8 @@ ALTER TABLE  `#__mams_articles` ADD  `art_modified_by` INT NOT NULL AFTER  `art_
 ALTER TABLE  `#__mams_articles` ADD  `checked_out` INT NOT NULL AFTER  `art_modified_by` ,
 ADD  `checked_out_time` DATETIME NOT NULL AFTER  `checked_out` ;
 ALTER TABLE  `#__mams_articles` ADD  `version` INT NOT NULL ;
-ALTER TABLE  `#__mams_articles` ADD  `asset_id` INT NOT NULL ,
-ADD INDEX (  `asset_id` ) ;
+ALTER TABLE  `#__mams_articles` ADD  `asset_id` INT NOT NULL , ADD INDEX (  `asset_id` ) ;
+ALTER TABLE  `#__mams_articles` CHANGE  `published`  `state` INT( 11 ) NOT NULL ;
 
 ALTER TABLE  `#__mams_secs` ADD  `metadata` TEXT NOT NULL ;
 ALTER TABLE  `#__mams_secs` ADD  `asset_id` INT NOT NULL ,
@@ -29,6 +29,7 @@ ALTER TABLE  `#__mams_artauth` ADD  `aa_field` INT NOT NULL DEFAULT  '5' AFTER  
 ALTER TABLE  `#__mams_artmed` ADD  `am_field` INT NOT NULL DEFAULT  '6' AFTER  `am_id` , ADD INDEX (  `am_field` );
 ALTER TABLE  `#__mams_artdl` ADD  `ad_field` INT NOT NULL DEFAULT  '7' AFTER  `ad_id` , ADD INDEX (  `ad_field` );
 ALTER TABLE  `#__mams_artlinks` ADD  `al_field` INT NOT NULL DEFAULT  '8' AFTER  `al_id` , ADD INDEX (  `al_field` );
+
 
 
 INSERT INTO `#__content_types` (`type_title`, `type_alias`, `table`, `rules`, `field_mappings`, `router`) VALUES
