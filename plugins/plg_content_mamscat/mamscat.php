0b38b3e2c8c4fd0edec50963ae2ad7a0ec77b627
Mike Amundsen - TLG <mamundsen@the-lynx-group.com>
2014-06-13 12:22:17 -0400

    plg_content_mamscat bug fix

--- plugins/plg_content_mamscat/mamscat.php
+++ plugins/plg_content_mamscat/mamscat.php
@@ -6,7 +6,7 @@ class  plgContentMAMSCat extends JPlugin
 
 	public function onContentPrepare($context, &$article, &$params, $limitstart) {
 		$regex = "#{mamscat}(.*?){/mamscat}#s";
-		$plugin =&JPluginHelper::getPlugin('content', 'MVid');
+		$plugin =&JPluginHelper::getPlugin('content', 'MAMSCat');
 		if (!$plugin->published){ 
 			//plugin not published 
 		}else  { 
@@ -20,7 +20,7 @@ class  plgContentMAMSCat extends JPlugin
 		
 				$cid = trim($matcheslist[0]);
 			
-				$new=$this->MAMSCatReplacer($cid);
+				if ($cid) $new=$this->MAMSCatReplacer($cid);
 				$article->text = preg_replace("|$match[0]|", $new, $article->text, 1);	
 			}
 		}
@@ -44,35 +44,37 @@ class  plgContentMAMSCat extends JPlugin
 		$query->where('ac.ac_cat = '.(int)$cid);
 		$query->where('ac.published >= 1');
 		$db->setQuery($query);
-		$artids = $db->loadResultArray(0);
+		$artids = $db->loadColumn(0);
 		
-		$query = $db->getQuery(true);
-		$query->select('a.*,s.sec_id,s.sec_name,s.sec_alias');
-		$query->from('#__mams_articles AS a');
-		$query->join('RIGHT','#__mams_secs AS s ON s.sec_id = a.art_sec');
-		$query->where('a.art_id IN ('.implode(",",$artids).')');
-		$query->where('a.published >= 1');
-		$query->where('a.access IN ('.implode(",",$alvls).')');
-		if (!in_array($cfg->ovgroup,$alvls)) { $query->where('a.art_publish_up <= NOW()'); $query->where('(a.art_publish_down >= NOW() || a.art_publish_down="0000-00-00")'); }
-		$query->order('a.art_publish_up DESC, s.ordering ASC, a.ordering ASC');
-		$limitstart = 0;
-		$limit = 5;
-		
-		$db->setQuery($query, $limitstart, $limit);
-		$items = $db->loadObjectList();
-		
-		$output = "";
-		$output .=  '<ul>';
-		foreach ($items as $a) {
-			$output .= '<li>';
-			$output .= '<span class="mams-catplug-title">';
-			$output .= '<a href="'.JRoute::_("index.php?option=com_mams&view=article&secid=".$a->sec_id.":".$a->sec_alias."&artid=".$a->art_id.":".$a->art_alias).'">';
-			$output .= $a->art_title;
-			$output .= '</a></span>';
-
-			$output .= '</li>';
+		if ($artids) {
+			$query = $db->getQuery(true);
+			$query->select('a.*,s.sec_id,s.sec_name,s.sec_alias');
+			$query->from('#__mams_articles AS a');
+			$query->join('RIGHT','#__mams_secs AS s ON s.sec_id = a.art_sec');
+			$query->where('a.art_id IN ('.implode(",",$artids).')');
+			$query->where('a.state >= 1');
+			$query->where('a.access IN ('.implode(",",$alvls).')');
+			if (!in_array($cfg->ovgroup,$alvls)) { $query->where('a.art_publish_up <= NOW()'); $query->where('(a.art_publish_down >= NOW() || a.art_publish_down="0000-00-00")'); }
+			$query->order('a.art_publish_up DESC, s.ordering ASC, a.ordering ASC');
+			$limitstart = 0;
+			$limit = 5;
+			
+			$db->setQuery($query, $limitstart, $limit);
+			$items = $db->loadObjectList();
+			
+			$output = "";
+			$output .=  '<ul>';
+			foreach ($items as $a) {
+				$output .= '<li>';
+				$output .= '<span class="mams-catplug-title">';
+				$output .= '<a href="'.JRoute::_("index.php?option=com_mams&view=article&secid=".$a->sec_id.":".$a->sec_alias."&artid=".$a->art_id.":".$a->art_alias).'">';
+				$output .= $a->art_title;
+				$output .= '</a></span>';
+	
+				$output .= '</li>';
+			}
+			$output .= '</ul>';
 		}
-		$output .= '</ul>';
 		return $output;
 	}
 }
