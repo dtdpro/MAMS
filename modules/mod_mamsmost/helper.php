17b228c4e98b8c520dc190abe6af28ee9a880694
DtD Productions <mike@dtdpro.com>
2015-06-24 15:08:25 -0400

    New Module - Most Read Articles

---
+++ modules/mod_mamsmost/helper.php
@@ -0,0 +1,153 @@
+<?php
+
+
+// no direct access
+defined('_JEXEC') or die;
+
+class modMAMSMostHelper
+{
+	static function getFeatured($params)
+	{
+		$db		= JFactory::getDbo();
+		$user = JFactory::getUser();
+		$cfg = MAMSHelper::getConfig();
+		
+		$alvls = Array();
+		$alvls = $user->getAuthorisedViewLevels();
+		$alvls = array_merge($alvls,$cfg->reggroup);
+		
+		$query	= $db->getQuery(true);
+
+		$query->select('a.*,s.*,count(*) as arthits');
+        $query->from('#__mams_track AS t');
+		$query->join('LEFT','#__mams_articles AS a ON t.mt_item = a.art_id');
+		$query->join('RIGHT','#__mams_secs AS s ON s.sec_id = a.art_sec');
+		$query->where('a.feataccess IN ('.implode(",",$user->getAuthorisedViewLevels()).')');
+		$query->where('a.state >= 1');
+		if (!in_array($cfg->ovgroup,$alvls)) { $query->where('a.art_publish_up <= NOW()'); $query->where('(a.art_publish_down >= NOW() || a.art_publish_down="0000-00-00")'); }
+        $query->where('a.art_publish_up >= DATE_SUB(NOW(), INTERVAL '.$params->get('num_days_old',90).' day)');
+        $query->where('t.mt_type = "article"');
+        $query->where('t.mt_time >= DATE_SUB(NOW(), INTERVAL '.$params->get('num_days_read',30).' day)');
+        $query->group('t.mt_item');
+        $query->order('arthits DESC');
+        $db->setQuery($query,0,$params->get('count',5));
+        $items = $db->loadObjectList();
+								
+		foreach ($items as &$i) {
+			
+			//Authors
+			$i->auts = modMAMSMostHelper::getFieldAuthors($i->art_id,"5",$alvls);
+			
+			//Categories
+			$qc=$db->getQuery(true);
+			$qc->select('c.cat_id,c.cat_title,c.cat_alias');
+			$qc->from('#__mams_artcat as ac');
+			$qc->join('RIGHT','#__mams_cats AS c ON ac.ac_cat = c.cat_id');
+			$qc->where('ac.published >= 1');
+			$qc->where('c.published >= 1');
+			$qc->where('c.access IN ('.implode(",",$alvls).')');
+			$qc->where('ac.ac_art = '.$i->art_id);
+			$qc->order('ac.ordering ASC');
+			$db->setQuery($qc);
+			$i->cats=$db->loadObjectList();
+			
+			if ($i->art_fielddata)
+			{
+				$registry = new JRegistry;
+				$registry->loadString($i->art_fielddata);
+				$i->art_fielddata = $registry->toObject();
+			}
+			if ($params->get('show_allfields',0)) {
+				$i->fields = modMAMSMostHelper::getArticleListFields($i->art_id,$alvls);
+			}
+		}
+		
+		return $items;
+	}
+	
+	protected function getArticleListFields($artid,$alvls) {
+		$db =& JFactory::getDBO();
+		$query = $db->getQuery(true);
+		
+		$query->select('*,f.params as field_params,g.params as group_params');
+		$query->from("#__mams_article_fields as f");
+		$query->select('g.group_title');
+		$query->join('LEFT', '#__mams_article_fieldgroups AS g ON g.group_id = f.field_group');
+		$query->where('f.published >= 1');
+		$query->where('f.access IN ('.implode(",",$alvls).')');
+		$query->where('g.published >= 1');
+		$query->where('g.access IN ('.implode(",",$alvls).')');
+		$query->where('f.field_show_module = 1');
+		$query->where('f.field_id >= 100');
+		$query->order('f.ordering ASC');
+		$db->setQuery($query);
+		$items = $db->loadObjectList();
+		
+		foreach ($items as &$i) {
+			switch ($i->field_type) {
+				case "auths": $i->data = modMAMSMostHelper::getFieldAuthors($artid,$i->field_id,$alvls); break;
+				case "dloads": $i->data = modMAMSMostHelper::getFieldDownloads($artid,$i->field_id,$alvls); break;
+				case "links": $i->data = modMAMSMostHelper::getFieldLinks($artid,$i->field_id,$alvls); break;
+			}
+			
+			$registryf = new JRegistry;
+			$registryf->loadString($i->field_params);
+			$i->field_params = $registryf->toObject();
+			
+			$registryg = new JRegistry;
+			$registryg->loadString($i->group_params);
+			$i->group_params = $registryg->toObject();
+		}
+			
+		return $items;
+	}
+	
+	protected function getFieldAuthors($artid, $fid, $alvls) {
+		$db =& JFactory::getDBO();
+		$qa=$db->getQuery(true);
+		$qa->select('a.auth_id,a.auth_fname,a.auth_mi,a.auth_lname,a.auth_titles,a.auth_alias,a.auth_sec');
+		$qa->from('#__mams_artauth as aa');
+		$qa->join('RIGHT','#__mams_authors AS a ON aa.aa_auth = a.auth_id');
+		$qa->where('aa.published >= 1');
+		$qa->where('a.published >= 1');
+		$qa->where('a.access IN ('.implode(",",$alvls).')');
+		$qa->where('aa.aa_art = '.$artid);
+		$qa->where('aa.aa_field = '.$fid);
+		$qa->order('aa.ordering ASC');
+		$db->setQuery($qa);
+		return $db->loadObjectList();
+	}
+	
+	protected function getFieldDownloads($artid, $fid, $alvls) {
+		$db =& JFactory::getDBO();
+		$qa=$db->getQuery(true);
+		$qa->select('d.*');
+		$qa->from('#__mams_artdl as ad');
+		$qa->join('RIGHT','#__mams_dloads AS d ON ad.ad_dload = d.dl_id');
+		$qa->where('ad.published >= 1');
+		$qa->where('d.published >= 1');
+		$qa->where('d.access IN ('.implode(",",$alvls).')');
+		$qa->where('ad.ad_art = '.$artid);
+		$qa->where('ad.ad_field = '.$fid);
+		$qa->order('ad.ordering ASC');
+		$db->setQuery($qa);
+		return $db->loadObjectList();
+	}
+	
+	protected function getFieldLinks($artid, $fid, $alvls) {
+		$db =& JFactory::getDBO();
+		$qa=$db->getQuery(true);
+		$qa->select('l.*');
+		$qa->from('#__mams_artlinks as al');
+		$qa->join('RIGHT','#__mams_links AS l ON al.al_link = l.link_id');
+		$qa->where('al.published >= 1');
+		$qa->where('l.published >= 1');
+		$qa->where('l.access IN ('.implode(",",$alvls).')');
+		$qa->where('al.al_art = '.$artid);
+		$qa->where('al.al_field = '.$fid);
+		$qa->order('al.ordering ASC');
+		$db->setQuery($qa);
+		return $db->loadObjectList();
+	}
+
+}
---
+++ modules/mod_mamsmost/tmpl/default.php
@@ -0,0 +1,177 @@
+<?php
+
+// no direct access
+defined('_JEXEC') or die;
+
+JHtml::_('behavior.keepalive');
+echo '<div class="mams-featmod">';
+$firstart=true;
+foreach ($articles as $a) {
+	echo '<div class="mams-featmod-article';
+	if ($firstart) { echo ' first-child'; $firstart=false; }
+	echo '">';
+	if ($a->art_thumb && $params->get('show_thumb',0)) {
+		echo '<div class="mams-featmod-thumb">';
+		echo '<a href="'.JRoute::_("index.php?option=com_mams&view=article&secid=".$a->sec_id.":".$a->sec_alias."&artid=".$a->art_id.":".$a->art_alias).'">';
+		echo '<img border="0" class="mams-featmod-artthumb" src="'.$a->art_thumb.'" /></a>';
+		echo '</div>';
+	}
+	if ($a->art_thumb && $params->get('show_thumb',0)) echo '<div class="mams-featmod-articleinfowt">';
+	else echo '<div class="mams-featmod-articleinfo">';
+	echo '<div class="mams-featmod-title">';
+	echo '<a href="'.JRoute::_("index.php?option=com_mams&view=article&secid=".$a->sec_id.":".$a->sec_alias."&artid=".$a->art_id.":".$a->art_alias).'">';
+	echo $a->art_title;
+	echo '</a></div>';
+	if ($a->auts && $params->get('show_author',1)) {
+		$auts = Array();
+		foreach ($a->auts as $f) {
+			if ($params->get('link_pubinfo',0)) $auts[]='<a href="'.JRoute::_("index.php?option=com_mams&view=author&secid=".$f->auth_sec."&autid=".$f->auth_id.":".$f->auth_alias).'" class="mams-artlist-autlink">'.$f->auth_fname.(($f->auth_mi) ? " ".$f->auth_mi : "")." ".$f->auth_lname.(($f->auth_titles) ? ", ".$f->auth_titles : "").'</a>';
+			else $auts[]=$f->auth_fname.(($f->auth_mi) ? " ".$f->auth_mi : "")." ".$f->auth_lname.(($f->auth_titles) ? ", ".$f->auth_titles : "");
+		}
+		echo '<div class="mams-featmod-author">';
+		echo implode(", ",$auts);
+		echo '</div>';
+	}
+	if ($params->get('show_pubinfo',1)) {
+		//Section Link
+		echo '<div class="mams-featmod-pubinfo">';
+		if ($params->get('show_pubsec',1)) {
+			
+			echo '<span class="mams-featmod-sec">';
+			if ($params->get('link_pubinfo',0)) echo '<a href="'.JRoute::_("index.php?option=com_mams&view=artlist&layout=section&secid=".$a->sec_id.":".$a->sec_alias).'" class="mams-artlist-seclink">';
+			echo '<em>'.$a->sec_name.'</em>';
+			if ($params->get('link_pubinfo',0)) echo '</a>';
+			echo '</span>';
+		}
+		
+		//Pub Date
+		if ($params->get('show_pubdate',1)) {
+			echo '<span class="mams-featmod-pubdate">';
+			echo ' published on <strong>';
+			echo date("F j, Y",strtotime($a->art_publish_up));
+			echo '</strong>';
+			echo '</span>';
+		}
+		
+		//Cat Links
+		if ($a->cats && $params->get('show_pubcat',1)) {
+			echo '<span class="mams-featmod-cat">';
+            if ($params->get('show_pubdate',1)) {
+                echo ' in <em>';
+            } elseif ($params->get('show_pubsec',1)) {
+                echo ' - <em>';
+            }
+            $cats = Array();
+			foreach ($a->cats as $c) {
+				if ($params->get('link_pubinfo',0)) $cats[]='<a href="'.JRoute::_("index.php?option=com_mams&view=artlist&layout=category&catid=".$c->cat_id.":".$c->cat_alias).'" class="mams-artlist-catlink">'.$c->cat_title.'</a>'; //&secid=".$a->sec_id.":".$a->sec_alias."
+				else $cats[]=$c->cat_title;
+			}
+			echo implode(", ",$cats);
+			echo '</em>';
+			echo '</span>';
+		}
+		echo '</div>';
+	}
+		
+	//Description
+	if ($params->get('show_desc',0)) {
+		echo '<div class="mams-featmod-desc">';
+		echo $a->art_desc;
+		echo '</div>';
+	}
+	
+	//Additional Fields
+	if ($params->get('show_allfields',0)) {
+		if ($a->fields) {
+			$curgroup = "";
+			$first=true;
+			foreach ($a->fields as $f) {
+				$fn = $f->field_name;
+				if ($a->art_fielddata->$fn || $f->data) {
+					if ($f->group_name != $curgroup) {
+						if (!$first) { echo '</div>';  }
+						else { $first=false; }
+						echo '<div class="mams-featmod-'.$f->group_name.'">';
+						$curgroup = $f->group_name;
+						if ($f->group_show_title) {
+							echo '<div class="mams-featmod-'.$f->group_name.'-title">';
+							if ($f->group_params->linktitlemod) echo '<a href="'.JRoute::_("index.php?option=com_mams&view=article&secid=".$a->sec_id.":".$a->sec_alias."&artid=".$a->art_id.':'.$a->art_alias).'#'.$f->group_name.'">';
+							echo $f->group_title;
+							if ($f->group_params->linktitlemod) echo '</a>';
+							echo '</div>';
+						}
+					}
+					if ($f->field_params->show_title_desc) {
+						echo '<div class="mams-featmod-'.$f->group_name.'-'.$f->field_name.'-title">';
+						echo $f->field_title;
+						echo '</div>';
+					}
+					echo '<div class="mams-featmod-'.$f->group_name.'-'.$f->field_name.'">';
+					if ($f->field_params->pretext) echo $f->field_params->pretext.' ';
+					switch ($f->field_type) {
+						case "textfield":
+							if ($f->field_params->linktext==1) echo '<a href="'.JRoute::_("index.php?option=com_mams&view=article&secid=".$a->sec_id.":".$a->sec_alias."&artid=".$a->art_id.':'.$a->art_alias).'#'.$f->field_name.'">';
+							if ($f->field_params->linktext==2) echo '<a href="'.JRoute::_("index.php?option=com_mams&view=article&secid=".$a->sec_id.":".$a->sec_alias."&artid=".$a->art_id.':'.$a->art_alias).'#'.$f->group_name.'">';
+							echo $a->art_fielddata->$fn;
+							if ($f->field_params->linktext) echo '</a>';
+							break;
+						case "textbox":
+						case "editor":
+							echo $a->art_fielddata->$fn;
+							break;
+						case "auths":
+							$auts = Array();
+							foreach ($f->data as $d) {
+								if ($params->get('link_pubinfo',0)) { $auts[]='<a href="'.JRoute::_("index.php?option=com_mams&view=author&secid=".$d->auth_sec."&autid=".$d->auth_id.":".$d->auth_alias).'" class="mams-featmod-'.$f->group_name.'-'.$f->field_name.'-autlink">'.$d->auth_fname.(($d->auth_mi) ? " ".$d->auth_mi : "")." ".$d->auth_lname.(($d->auth_titles) ? ", ".$d->auth_titles : "").'</a>'; }
+								else { $auts[] = $d->auth_fname.(($d->auth_mi) ? " ".$d->auth_mi : "")." ".$d->auth_lname.(($d->auth_titles) ? ", ".$d->auth_titles : ""); }
+							}
+							echo implode(", ",$auts);
+							break;
+						case "dloads":
+							$firstdl=true;
+							foreach ($f->data as $d) {
+								echo '<div class="mams-featmod-'.$f->group_name.'-'.$f->field_name.'-dload">';
+								echo '<a href="'.JRoute::_("components/com_mams/dl.php?dlid=".$d->dl_id).'" ';
+								echo 'target="_blank" ';
+								echo 'class="mams-featmod-'.$f->group_name.'-'.$f->field_name.'-artdload';
+								if ($firstdl) { echo ' firstdload'; $firstdl=false; }
+								echo '">';
+								echo 'Download '.$d->dl_lname;
+								echo '</a>';
+								echo '</div>';
+							}
+							break;
+						case "links":
+							$firstlink=true;
+							foreach ($f->data as $d) {
+								echo '<div class="mams-featmod-'.$f->group_name.'-'.$f->field_name.'-link">';
+								echo '<a href="'.$d->link_url.'" ';
+								echo 'target="'.$d->link_target.'" ';
+								echo 'class="mams-featmod-'.$f->group_name.'-'.$f->field_name.'-artlink';
+								if ($firstlink) { echo ' firstlink'; $firstlink=false; }
+								echo '">';
+								echo $d->link_title;
+								echo '</a>';
+								echo '</div>';
+							}
+							break;
+					}
+					if ($f->field_params->posttext) echo ' '.$f->field_params->posttext;
+					echo '</div>';
+				}
+			}
+			if (!$first) echo '</div>';
+		}
+	}
+	
+	//Readmore
+	if ($params->get('show_readmore',0)) {
+		echo '<div class="mams-featmod-readmore">';
+		echo '<a href="'.JRoute::_("index.php?option=com_mams&view=article&secid=".$a->sec_id.":".$a->sec_alias."&artid=".$a->art_id.":".$a->art_alias).'" class="mams-featmod-artlink read-more uk-button">';
+		echo $params->get('text_readmore',"Read More");
+		echo '</a></div>';
+	}
+	echo '</div>';
+	echo '</div>';
+}
+echo '</div>';
