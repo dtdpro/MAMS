a4e272df8446705fe34e47d75f69c727ae8e3d0b
Mike Amundsen <mike@dtdpro.com>
2019-09-17 11:49:38 -0400

    sql fix

--- component/admin/sql/updates/2.10.2.sql
+++ component/admin/sql/updates/2.10.2.sql
@@ -1,2 +1,1 @@
 ALTER TABLE `#__mams_article_fields` ADD `field_show_author` BOOLEAN NOT NULL DEFAULT TRUE AFTER `field_show_module`;
-ALTER TABLE `#__mams_tags` ADD `tag_icon` VARCHAR(255) NOT NULL AFTER `tag_image`;
