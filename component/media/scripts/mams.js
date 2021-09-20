c817e74b0dd6ea6cc75184b02044ecef2de5e017
Mike Amundsen <mike@dtdpro.com>
2021-09-20 11:38:20 -0400

    GA Video Percent Played Event

--- component/media/scripts/mams.js
+++ component/media/scripts/mams.js
@@ -23,6 +23,21 @@ function MAMSTrackMedia(item_id) {
 
 	$.extend(MediaElementPlayer.prototype, {
 		buildmamsgoogleanalytics: function(player, controls, layers, media) {
+
+			var i = 0;
+
+			// Define at which percentages you want to fire an event
+			var markers = [25,50,75,90,100];
+			var playersMarkers = [];
+
+			function findObjectIndexById(haystack, key, needle) {
+				for (var i = 0; i < haystack.length; i++) {
+					if (haystack[i][key] == needle) {
+						return i;
+					}
+				}
+				return null;
+			}
 				
 			media.addEventListener('loadeddata', function() {
 				if (typeof ga != 'undefined') {
@@ -30,6 +45,12 @@ function MAMSTrackMedia(item_id) {
 				}
 				if (typeof _gaq != 'undefined') {
 					_gaq.push(['_trackEvent','MAMSMedia','Loaded',player.options.videoExtTitle]);
+				}
+				if (typeof gtag === 'function') {
+					gtag('event', 'Loaded', {
+						'event_category': 'MAMSMedia',
+						'event_label': player.options.videoExtTitle
+					});
 				}
 			}, false);
 				
@@ -40,6 +61,12 @@ function MAMSTrackMedia(item_id) {
 				if (typeof _gaq != 'undefined') {
 					_gaq.push(['_trackEvent','MAMSMedia','Play',player.options.videoExtTitle]);
 				}
+				if (typeof gtag === 'function') {
+					gtag('event', 'Play', {
+						'event_category': 'MAMSMedia',
+						'event_label': player.options.videoExtTitle
+					});
+				}
 			}, false);
 			
 			media.addEventListener('pause', function() {
@@ -48,6 +75,12 @@ function MAMSTrackMedia(item_id) {
 				}
 				if (typeof _gaq != 'undefined') {
 					_gaq.push(['_trackEvent','MAMSMedia','Pause',player.options.videoExtTitle]);
+				}
+				if (typeof gtag === 'function') {
+					gtag('event', 'Pause', {
+						'event_category': 'MAMSMedia',
+						'event_label': player.options.videoExtTitle
+					});
 				}
 			}, false);	
 			
@@ -58,7 +91,40 @@ function MAMSTrackMedia(item_id) {
 				if (typeof _gaq != 'undefined') {
 					_gaq.push(['_trackEvent','MAMSMedia','Ended',player.options.videoExtTitle]);
 				}
+				if (typeof gtag === 'function') {
+					gtag('event', 'Ended', {
+						'event_category': 'MAMSMedia',
+						'event_label': player.options.videoExtTitle
+					});
+				}
+
 			}, false);
+
+			media.addEventListener('timeupdate', function(e) {
+				var percentPlayed = Math.floor(player.media.currentTime*100/player.media.duration);
+				//var playerMarkerIndex = findObjectIndexById(playersMarkers,'id', player.id);
+
+				if(markers.indexOf(percentPlayed)>-1 && playersMarkers.indexOf(percentPlayed)==-1)
+				{
+					playersMarkers.push(percentPlayed);
+
+					if (typeof ga != 'undefined') {
+						ga('send', 'event', 'MAMSMedia', percentPlayed.toString()+"%", player.options.videoExtTitle);
+					}
+					if (typeof _gaq != 'undefined') {
+						_gaq.push(['_trackEvent','MAMSMedia',percentPlayed.toString()+"%",player.options.videoExtTitle]);
+					}
+					if (typeof gtag === 'function') {
+						gtag('event', percentPlayed.toString()+"%", {
+							'event_category': 'MAMSMedia',
+							'event_label': player.options.videoExtTitle
+						});
+					}
+				}
+			},false);
+
+			i++;
+
 		}
 	});
 		
